de15fe934deecc4b17acd188c0a8bac06e9391a2
Axis <77634274+blankRiot96@users.noreply.github.com>
2022-09-14 00:00:52 +0530

    failed collision attemt

--- game/entities/player.py
+++ game/entities/player.py
@@ -42,17 +42,6 @@ class Player(CollidableEntity):
         return pos
 
 
-    # def collides(self, other: MovingEntity, dt: float) -> bool:
-    #     predicted_pos = self.predicted_pos(dt)
-    #     predicted_rect = pygame.Rect(predicted_pos, self.SIZE)
-    #     is_colliding = predicted_rect.colliderect(other.rect)
-    #     if is_colliding:
-    #         self.colliding_with = other.type
-    #     else:
-    #         self.colliding_with = None
-
-    #     return is_colliding, predicted_pos
-
     def move(self, dv: pygame.Vector2) -> None:
         self.pos += dv 
         self.rect.topleft = self.pos
--- game/states/world.py
+++ game/states/world.py
@@ -104,17 +104,15 @@ class PlayerStage(SpikeStage):
         self.player.handle_input(event_info["events"])
         
         # Handle Player-Platform collision 
-        self.player.vel = self.player.VEL
         for plat in self.platforms:
             if self.player.collides(plat) and not self.player.is_space_pressed:
-                self.player.vel = 0
+                horizontal_diff = self.player.pos.x - plat.pos.x
+                if horizontal_diff > 0:
+                    self.player.pos.x += horizontal_diff
+                elif horizontal_diff < 0:
+                    self.player.pos.x -= horizontal_diff
 
-                if self.player.pos.x - plat.pos.x > 0:
-                    self.player.rect.left = plat.rect.right 
-                elif self.player.pos.x - plat.pos.x < 0:
-                    self.player.rect.right = plat.rect.left 
-
-                break
+                return
 
         self.player.update(event_info["dt"])
 
